d355190c2e5c0b8aee3005514ec6a7c0f9f02805
pakhi <pakhivashis182001@gmail.com>
2023-12-17 18:24:30 +0530

    files added

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import "gofr.dev/pkg/gofr"
 
 func main() {
-    app := gofr.New()
+	app := gofr.New()
 
-    app.GET("/", func(ctx *gofr.Context) (interface{}, error) {
-        return "Hello World!", nil
-    })
+	app.GET("/", func(ctx *gofr.Context) (interface{}, error) {
+		return "Hello World!", nil
+	})
 
-    app.Start()
+	app.Start()
 }
